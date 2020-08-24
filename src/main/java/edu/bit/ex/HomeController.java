97f89ee03bf8716132de5df523124e8bdee53165
tom heo <nqwrt@naver.com>
2020-08-24 09:54:12 +0900

    version2

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 //version-1
+//version-2
 @Controller
 public class HomeController {
 	
