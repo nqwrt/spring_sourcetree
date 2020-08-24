def46355dca5d9ff1e57c2bce88f51306785800e
tom heo <nqwrt@naver.com>
2020-08-24 09:51:31 +0900

    version1

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//version-1
 @Controller
 public class HomeController {
 	
@@ -33,7 +34,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		//¸ÓÁö1
+		//ï¿½ï¿½ï¿½ï¿½1
 		return "home";
 	}
 	
