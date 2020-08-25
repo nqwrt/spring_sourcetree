4c8bbb98d2d26644de4e6e47f9bb84351e2583e0
tom heo <nqwrt@naver.com>
2020-08-25 09:39:57 +0900

    //develop-2

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//develop-2
 //develop-1
 @Controller
 public class HomeController {
